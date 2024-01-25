53f5886c2864af7ea0b0ac1bb88dca678f036fc5
Suraj <mouryasuraj6266444@gmail.com>
2024-01-25 19:46:57 +0530

    COmpleted project

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
 
 
 // for fixed IMage
-const fixedImage = document.querySelector(".fixedImage");
+const fixedImage = document.querySelector("#fixedImage");
 const productLst = document.querySelector(".product-list");
 
 const elems = document.querySelectorAll(".elem");
 elems.forEach((e)=>{
     e.addEventListener("mouseenter", ()=>{
+        const image = e.getAttribute("data-image")
         fixedImage.style.display = "block"
+        fixedImage.style.backgroundImage = `url(${image })`
+    })
+    e.addEventListener("mouseleave", ()=>{
+        fixedImage.style.display = "none"
     })
 })
